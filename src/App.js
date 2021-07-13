320dabc735585b62f0d61a0eded1e98a10294e22
jinhyo <sosilion@naver.com>
2021-07-13 21:45:20 +0900

    base setting
    - react-create-app
    - react router 설치
    - material UI 세팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button, TableRow } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>asdf</Button>
+    </div>
+  );
+}
+
+export default App;

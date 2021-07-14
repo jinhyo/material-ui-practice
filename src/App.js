c4a194907203fca25c073f9fd7b8f478cd080bf8
jinhyo <sosilion@naver.com>
2021-07-14 15:35:09 +0900

    <Header />
    - component : ElevationScroll, AppBar, Toolbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { Button, TableRow } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import Header from "./components/ui/Header";
 
 function App() {
   return (
     <div className="App">
-      <Button>asdf</Button>
+      <Header />
+      {[...new Array(120)]
+        .map(
+          () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+        )
+        .join("\n")}
+      <Button>123</Button>
     </div>
   );
 }
---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,25 @@
+import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+import React from "react";
+
+function ElevationScroll({ children }) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true, // true일 경우 위 아래 스크롤할 때 다 적용됨, false일 경우에는 아래로 스크롤할 때만 적용됨
+    threshold: 10,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 14 /* 숫자는 음영이 지는 범위 */ : 0,
+  });
+}
+
+function Header() {
+  return (
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>asd1</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
+
+export default Header;
